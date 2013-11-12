dde1aa89d7b4f59728f84b589d927cf6394294c1
Strelnikov Ivan <xplk90@gmail.com>
2013-11-12 15:46:15 +0700

    hotfix ...

--- src/url/urlToPath.js
+++ src/url/urlToPath.js
@@ -14,13 +14,15 @@ urlToPath.getPath = function(url) {
 	
 	var path = [this.basePath, url.hostname];
 	path = path.concat(decodeURIComponent(url.pathname).split('/').slice(1));
-	if (path[path.length - 1] === '') {
-		// URL ends with a slash. It is a folder
-		path[path.length - 1] = 'index.html';
-	} else if (path[path.length - 1].indexOf('.') === -1) {
-		// Last part looks like a folder
-		path.push('index.html');
-	}
+        if (path[path.length - 1] === '' || path[path.length - 1] == null) {
+            // URL ends with a slash. It is a folder
+            path[path.length - 1] = 'index.html';
+        } else if (path[path.length-1] != null && path[path.length - 1].indexOf('.') === -1 ) {
+            // Last part looks like a folder
+            path.push('index.html');
+        }else{
+            path[path.length - 1] = 'index__.html'
+        }
 	return pathModule.join.apply(null, path);
 };
 
